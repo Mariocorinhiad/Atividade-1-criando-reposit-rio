2408247d0b58c797e97c02fac2da34113ec7f9dd
Mario Levino <144383929+Mariocorinhiad@users.noreply.github.com>
2024-02-29 14:39:59 -0300

    Atividade 1
    Atividade em Java no apache

---
+++ Atividade1.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package com.mycompany.atividade1;
+
+/**
+ *
+ * @author mario
+ */
+public class Atividade1 {
+
+    public static void main(String[] args) {
+        System.out.println("Atividade 1 ");
+    
+
+Relogio R1 = new Relogio("Tipo 1");
+Relogio R2 = new Relogio("Tipo 2");
+Relogio R3 = new Relogio("Tipo 3");
+Relogio R4 = new Relogio("Tipo 4");
+
+// Marcas de relogio
+
+R1.marca = "Tecnos";
+R2.marca = "Rolex";
+R3.marca = "Omega";
+R4.marca = "Hublot";
+
+system.out.println(R1.marca);
+system.out.println(R2.marca);
+system.out.println(R3.marca);
+system.out.println(R4.marca);
+
+system.out.println(Relogio.Valor)
+
+        
+system.out.println(R1.marca + "Valor" + R1.Valor)
+system.out.println(R2.marca + "Valor" + R2.Valor)                
+system.out.println(R3.marca + "Valor" + R3.Valor)        
+system.out.println(R4.marca + "Valor" + R4.Valor)        
+        }
+}
